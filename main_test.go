ed0757b149901f96caa4a86122c7b520e5c8d533
Gasoid <gasoid@gmail.com>
2023-03-28 20:04:16 +0200

    tests

---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	_ "github.com/Gasoid/photoDumper/docs"
+)
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{"Test main"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
